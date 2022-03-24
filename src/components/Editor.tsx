f509e2a2762363b17cb6b0933e24a765ad74ecf1
chezhe <chezhe@hey.com>
2022-03-24 17:17:39 +0800

    optimize: state management

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useRef, useState } from 'react'
+import { Reducer, useEffect, useReducer, useRef, useState } from 'react'
 import { DEFAULT_INPUT } from '../constants'
 import { parseBySimpleMarkdown } from '../utils/parser'
 import MindMap from './MindMap'
@@ -6,27 +6,40 @@ import isEqual from 'lodash.isequal'
 import { ConnectionLineType, MapTree } from '../type'
 import Controls from './Controls'
 
-const initialState = { connections: [] }
-
-type State = {
+type EditorState = {
   connections: string[]
+  isAnimate: boolean
+  lineType: ConnectionLineType
 }
 
-enum ConnectionAction {
-  new = 'new',
-  clear = 'clear',
-}
-type Action = {
-  type: ConnectionAction
-  payload: string
+enum EditorAction {
+  NEW_CONNECTION = 'NEW_CONNECTION',
+  RESET_CONNECTION = 'RESET_CONNECTION',
+  TOGGLE_ANIMATE = 'TOGGLE_ANIMATE',
+  UPDATE_LINE_TYPE = 'UPDATE_LINE_TYPE',
 }
 
-function reducer(state: State, action: Action) {
+type ReducerAction = {
+  type: EditorAction
+  payload?: any
+}
+
+const initialState: EditorState = {
+  connections: [],
+  isAnimate: true,
+  lineType: ConnectionLineType.CURVED,
+}
+
+const reducer: Reducer<EditorState, ReducerAction> = (state, action) => {
   switch (action.type) {
-    case 'new':
-      return { connections: [...state.connections, action.payload] }
-    case 'clear':
-      return { connections: [] }
+    case EditorAction.NEW_CONNECTION:
+      return { ...state, connections: [...state.connections, action.payload] }
+    case EditorAction.RESET_CONNECTION:
+      return { ...state, connections: [] }
+    case EditorAction.TOGGLE_ANIMATE:
+      return { ...state, isAnimate: action.payload, connections: [] }
+    case EditorAction.UPDATE_LINE_TYPE:
+      return { ...state, lineType: action.payload, connections: [] }
     default:
       throw new Error()
   }
@@ -36,18 +49,14 @@ export default function Editor() {
   const containerRef = useRef<SVGSVGElement | null>(null)
   const [input, setInput] = useState(DEFAULT_INPUT)
   const [mapTrees, setMapTrees] = useState<MapTree[]>([])
-  const [lineType, setLineType] = useState<ConnectionLineType>(
-    ConnectionLineType.CURVED
-  )
-  const [isAnimate, setIsAnimate] = useState(true)
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     const newMapTrees = parseBySimpleMarkdown(input)
     if (!isEqual(newMapTrees, mapTrees)) {
+      dispatch({ type: EditorAction.RESET_CONNECTION })
       setMapTrees(newMapTrees)
-      dispatch({ type: ConnectionAction.clear, payload: '' })
     }
   }, [input, mapTrees])
 
@@ -56,13 +65,20 @@ export default function Editor() {
       <div className="textarea-wrap">
         <textarea value={input} onChange={(e) => setInput(e.target.value)} />
         <Controls
-          lineType={lineType}
+          lineType={state.lineType}
           setLineType={(lineType) => {
-            setLineType(lineType)
-            dispatch({ type: ConnectionAction.clear, payload: '' })
+            dispatch({
+              type: EditorAction.UPDATE_LINE_TYPE,
+              payload: lineType,
+            })
           }}
-          isAnimate={isAnimate}
-          setIsAnimate={setIsAnimate}
+          isAnimate={state.isAnimate}
+          setIsAnimate={(isAnimate) => {
+            dispatch({
+              type: EditorAction.TOGGLE_ANIMATE,
+              payload: isAnimate,
+            })
+          }}
         />
       </div>
       <div className="map-wrap">
@@ -84,10 +100,14 @@ export default function Editor() {
                 parentRef={null}
                 containerRef={containerRef}
                 level={0}
-                line={{ type: lineType, isAnimate, color: '' }}
+                line={{
+                  type: state.lineType,
+                  isAnimate: state.isAnimate,
+                  color: '',
+                }}
                 appendConnection={(connection: string) => {
                   dispatch({
-                    type: ConnectionAction.new,
+                    type: EditorAction.NEW_CONNECTION,
                     payload: connection,
                   })
                 }}
